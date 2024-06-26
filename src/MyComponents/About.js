7b303fd5a2d27be26daf0d754b1520f9d23dd152
UnnatiVe <139119672+UnnatiVe@users.noreply.github.com>
2024-06-26 14:21:20 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      
+      <p> MY TODOS LIST is the ultimate to-do list website and task management app designed to boost your productivity and keep you organized. With an intuitive interface, customizable categories, and detailed task descriptions, managing your tasks has never been easier. Whether you're managing work projects, personal tasks, or home improvements, TaskMaster provides a seamless and intuitive experience to keep you on track and focused. </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './Footer.css'
+//how to use style sheet in react js
+const Footer = () => {
+ 
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">
+      Copyright &copy; MyTodosList.com
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
